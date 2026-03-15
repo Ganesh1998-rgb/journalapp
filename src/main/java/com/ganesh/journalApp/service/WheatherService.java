cb64226e6cf54b5ee18cc804e407a7d0dc6e59f8
Ganesh <gborase81@gmail.com>
2026-03-15 17:10:23 +0530

    Added the Redis and integrated the redis cloud in application

--- src/main/java/com/ganesh/journalApp/service/WheatherService.java
+++ src/main/java/com/ganesh/journalApp/service/WheatherService.java
@@ -14,8 +14,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @RequiredArgsConstructor
 public class WheatherService {
+
     private   final RestTemplate restTemplate;
     private final  WebClient webClient;
+private final RedisService redisService;
 
 @Value("${api.key}")
 private String apiKey;
@@ -23,25 +25,41 @@ private String apiKey;
 private final  AppCache appCache;
 
 
-public WheatherApiResponse getWheather(String city){
+public WheatherApiResponse getWheather(String city) {
+    WheatherApiResponse value = redisService.getValue("weather_of_" + city, WheatherApiResponse.class);
+    if (value != null) {
+        return value;
+    } else {
+        String replace = appCache.APP_CACHE.get(AppCache.Keys.WEATHER_API.name().toLowerCase()).replace(PlaceHolder.CITY, city).replace(PlaceHolder.API_KEY, apiKey);
 
-    String replace = appCache.APP_CACHE.get(AppCache.Keys.WEATHER_API.name().toLowerCase()).replace(PlaceHolder.CITY, city).replace(PlaceHolder.API_KEY, apiKey);
-
-    ResponseEntity<WheatherApiResponse> exchange = restTemplate.exchange(replace, HttpMethod.GET, null, WheatherApiResponse.class);
-    WheatherApiResponse body = exchange.getBody();
-    return body;
+        ResponseEntity<WheatherApiResponse> exchange = restTemplate.exchange(replace, HttpMethod.GET, null, WheatherApiResponse.class);
+        WheatherApiResponse body = exchange.getBody();
+        if(body!=null){
+            redisService.setValue("weather_of_" + city,body,300L);
+        }
+        return body;
+    }
 
 }
-
-
     public WheatherApiResponse getWeather(String city) {
-        String weatherApi = appCache.APP_CACHE.get(AppCache.Keys.WEATHER_API.name().toLowerCase()).replace(PlaceHolder.CITY, city).replace(PlaceHolder.API_KEY, apiKey);
-        return webClient
-                .get()
-                .uri(weatherApi)
-                .retrieve()
-                .bodyToMono(WheatherApiResponse.class)
-                .block();   // blocking only for testing
+        WheatherApiResponse value = redisService.getValue("weather_of_"+city, WheatherApiResponse.class);
+        if (value != null) {
+            return value;
+        }
+        else{
+            String weatherApi = appCache.APP_CACHE.get(AppCache.Keys.WEATHER_API.name().toLowerCase()).replace(PlaceHolder.CITY, city).replace(PlaceHolder.API_KEY, apiKey);
+            WheatherApiResponse block = webClient
+                    .get()
+                    .uri(weatherApi)
+                    .retrieve()
+                    .bodyToMono(WheatherApiResponse.class)
+                    .block();
+            if(block!=null){
+                redisService.setValue("weather_of_"+city,block,300L);
+            }
+            return block;
+        }
+        // blocking only for testing
     }
 
 }
